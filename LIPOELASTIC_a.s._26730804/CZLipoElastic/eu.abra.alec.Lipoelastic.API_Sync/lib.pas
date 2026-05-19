4ebf0b12987eb1fc0834a6a7a5c81eb446627d9c
marek-salava-abra <marek.salava@abra.eu>
2026-05-19 11:13:58 +0200

    doplnění starých parametrů

--- LIPOELASTIC_a.s._26730804/CZLipoElastic/eu.abra.alec.Lipoelastic.API_Sync/lib.pas
+++ LIPOELASTIC_a.s._26730804/CZLipoElastic/eu.abra.alec.Lipoelastic.API_Sync/lib.pas
@@ -48,9 +48,17 @@ begin
       mJSON.A['data'].O[0].D['IntrastatConstantWeight']:= ABO.GetFieldValueAsFloat('IntrastatCommodity_ID.ConstantWeight');
       mJSON.A['data'].O[0].S['EAN']:= ABO.GetFieldValueAsString('EAN');
       mJSON.A['data'].O[0].B['X_Matka']:= ABO.GetFieldValueAsBoolean('X_Matka');
+      mJSON.A['data'].O[0].B['TypProduktuCode']:= ABO.GetFieldValueAsBoolean('X_Typ_produktu.Code');
+      mJSON.A['data'].O[0].B['TypProduktuName']:= ABO.GetFieldValueAsBoolean('X_Typ_produktu.Name');
+      mJSON.A['data'].O[0].B['DruhCode']:= ABO.GetFieldValueAsBoolean('U_druh_ID.Code');
+      mJSON.A['data'].O[0].B['DruhName']:= ABO.GetFieldValueAsBoolean('U_druh_ID.Name');
+      mJSON.A['data'].O[0].B['VelikostCode']:= ABO.GetFieldValueAsBoolean('U_velikost_ID.Code');
+      mJSON.A['data'].O[0].B['VelikostName']:= ABO.GetFieldValueAsBoolean('U_velikost_ID.Name');
+      mJSON.A['data'].O[0].B['BarvaCode']:= ABO.GetFieldValueAsBoolean('U_barva_ID.Code');
+      mJSON.A['data'].O[0].B['BarvaName']:= ABO.GetFieldValueAsBoolean('U_barva_ID.Name');
       mJSON.A['data'].O[0].S['X_Parent_IDCode']:= ABO.GetFieldValueAsString('X_Parent_ID.Code');
       //KONEC ZÁKLADNÍCH DAT********************************************************************************
-      //doplnit
+
 
       //SEKCE JEDNOTEK********************************************************************************
       mUnits:=ABO.GetLoadedCollectionMonikerForFieldCode(ABO.GetFieldCode('StoreUnits'));
